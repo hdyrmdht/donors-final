ea5bbbecd8400f4945fd6d15dcc7f0de2a8ee77c
SaraAbdulhafiz <119739663+SaraAbdulhafiz@users.noreply.github.com>
2024-04-25 07:06:46 +0200

    profile

--- lib/share/network/remote/api_endpoints.dart
+++
@@ -1,7 +0,0 @@
-
-class ApiEndPoints {
-  static const String domainLink = 'http://127.0.0.1:8000/api/';
-  static const String login = "login";
-  static const String registerUser = "register";
-  static const String registerHospital = "register/company";
-}
